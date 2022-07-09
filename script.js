e8c4d3e7a5c1b6847dc18f66be9e66106f71b577
José Guillardi <63321040+JoseGu1llardi@users.noreply.github.com>
2022-07-09 15:04:26 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const company = document.querySelector('.company');
+
+console.log(company);
+
+company.addEventListener('click', function (e) {
+    alert('Website is currently in maintenance');
+});
